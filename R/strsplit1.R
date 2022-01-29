d61861fe5104defe6db49d9fa06db8466cf27b0c
Luka Pavlovic <pavloviclukap@gmail.com>
2022-01-29 18:48:39 +0100

    Updating everything at once xD

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Newthon-Raphson
+#'
+#' @param x its an ex
+#' @param split its split
+#'
+#' @return
+#' @export
+#'
+#' @examples x <- "alfa,bravo,charlie,delta"
+#' strsplit(x, split = ",")
+
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
